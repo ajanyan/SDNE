1e7e846d5204d102273ced7c11c9f115e1332d94
Binary <binary@localhost.localdomain>
2019-02-07 20:59:46 -0800

    Hello

--- sdne.py
+++ sdne.py
@@ -8,7 +8,6 @@ from keras import backend as K, regularizers
 from keras.models import Model
 from keras.layers import Dense, Embedding, Input, Reshape, Lambda
 
-
 class SNDE:
     __init__(self,graph,encode_dim, weight='weight', encoding_layer_dims=[] , beta=2, alpha=2, l2_param=1e-3):
 
