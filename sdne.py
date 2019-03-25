714ca314c206b19c5c0df1fabbe53a6d36c54e45
Paul P Joby <paulpjoby@gmail.com>
2019-03-25 10:11:12 +0530

    Here YOu GO

--- sdne.py
+++ sdne.py
@@ -3,8 +3,45 @@ from keras.layers import Dense
 import networkx as nx
 import numpy as np
 from keras.wrappers.scikit_learn import KerasClassifier
+from keras import backend as K, regularizers
 from keras.utils import np_utils
 from sklearn.preprocessing import LabelEncoder
+import math
+import networkx as nx
+import numpy as np
+from functools import reduce
+
+import keras
+from keras import backend as K, regularizers
+from keras.models import Model
+from keras.layers import Dense, Embedding, Input, Reshape, Lambda
+
+# Loss function
+def build_reconstruction_loss(beta):
+    """
+    return the loss function for 2nd order proximity
+
+    beta: the definition below Equation 3"""
+    assert beta > 1
+
+    def reconstruction_loss(true_y, pred_y):
+        diff = K.square(true_y - pred_y)
+
+        # borrowed from https://github.com/suanrong/SDNE/blob/master/model/sdne.py#L93
+        weight = true_y * (beta - 1) + 1
+
+        weighted_diff = diff * weight
+        return K.mean(K.sum(weighted_diff, axis=1))  # mean square error
+    return reconstruction_loss
+
+
+def edge_wise_loss(true_y, embedding_diff):
+    """1st order proximity
+    """
+    # true_y supposed to be None
+    # we don't use it
+    return K.mean(K.sum(K.square(embedding_diff), axis=1))  # mean square error
+
 
 g = nx.read_edgelist('dataset/karate.edgelist', create_using=nx.Graph())
 g = nx.convert_node_labels_to_integers(g)
@@ -17,6 +54,9 @@ embedding_dim = 9
 encode_dims = [34,20,16]
 decode_dims = [16,20,34]
 
+beta=2
+alpha=2
+l2_param=1e-3
 
 encoding_layer_dims = encode_dims
 decoding_layer_dims = decode_dims
@@ -42,8 +82,10 @@ for i, dim in enumerate(decoding_layer_dims):
             layer = Dense(dim, kernel_initializer='normal', activation='sigmoid',kernel_regularizer=regularizers.l2(l2_param), name='decoding-layer-{}'.format(i))
             model.add(layer)
 
-model.compile(loss='binary_crossentropy', optimizer='adadelta', metrics=['accuracy'])
-model.fit(adj_mat,adj_mat,epochs=1000)
+reconstruction_loss_function = build_reconstruction_loss(beta)
+
+model.compile(loss=reconstruction_loss_function, optimizer='adadelta', metrics=['accuracy'])
+model.fit(adj_mat,adj_mat,epochs=100)
 
 print("###############################################################################################")
 
